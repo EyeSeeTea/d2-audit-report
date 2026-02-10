d189637ab72f150edfb3b0d79713a542e3feba22
xurxodev <xurxosf@gmail.com>
2026-02-10 10:16:20 +0100

    Update sqlview if exists

--- src/data/utils/ensureSQLView.ts
+++ src/data/utils/ensureSQLView.ts
@@ -1,9 +1,10 @@
 import { D2Api } from "$/types/d2-api";
 import { FutureData, apiToFuture } from "$/data/api-futures";
 import sqlQuery from "$/data/sql-view/d2-audit-report.sql?raw";
-import { Future } from "$/domain/entities/generic/Future";
+//import sqlQuery from "$/data/sql-view/d2-audit-report-materialized.sql?raw";
 
 const SQL_VIEW_NAME = "d2-audit-report";
+//const SQL_VIEW_NAME = "d2-audit-report-materialized";
 
 export function ensureSQLView(api: D2Api): FutureData<string> {
     return apiToFuture(
@@ -17,17 +18,17 @@ export function ensureSQLView(api: D2Api): FutureData<string> {
         if (!existingView) {
             return createSqlView(api);
         } else {
-            //return updateSqlView(api, existingView.id);
-            return Future.success(existingView.id);
+            return updateSqlView(api, existingView.id);
+            //return Future.success(existingView.id);
         }
     });
 }
 
-// function updateSqlView(api: D2Api, viewId: string): FutureData<string> {
-//     const sqlViewDataToEdit = { ...sqlViewData, id: viewId };
+function updateSqlView(api: D2Api, viewId: string): FutureData<string> {
+    const sqlViewDataToEdit = { ...sqlViewData, id: viewId };
 
-//     return apiToFuture(api.models.sqlViews.put(sqlViewDataToEdit)).map(() => viewId);
-// }
+    return apiToFuture(api.models.sqlViews.put(sqlViewDataToEdit)).map(() => viewId);
+}
 
 function createSqlView(api: D2Api): FutureData<string> {
     return apiToFuture(api.models.sqlViews.post(sqlViewData)).map(response => response.uid);
